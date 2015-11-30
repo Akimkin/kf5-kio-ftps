cd724c15afcc8004ac8f62e175ad52da26129d8a
Denis Akimkin <akimkin.denis@gmail.com>
2015-11-30 18:46:29 +0200

    Code: Fixed time headers inclusion

--- ftp.cpp
+++ ftp.cpp
@@ -32,9 +32,6 @@
 #include "ftp.h"
 
 #include <sys/stat.h>
-#ifdef HAVE_SYS_TIME_H
-#include <sys/time.h>
-#endif
 #ifdef HAVE_SYS_SELECT_H
 #include <sys/select.h>
 #endif
@@ -52,8 +49,15 @@
 #include <unistd.h>
 #include <signal.h>
 
-#if TIME_WITH_SYS_TIME
+#ifdef TIME_WITH_SYS_TIME
+#include <sys/time.h>
 #include <time.h>
+#else
+#ifdef HAVE_SYS_TIME_H
+#include <sys/time.h>
+#else
+#include <time.h>
+#endif
 #endif
 
 #include <QtCore/QDir>
